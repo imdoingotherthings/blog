107cd0c411437a1823020bfbd4c788fb07742b6f
Matthew W. Ray <matt24ray@gmail.com>
2019-05-18 14:23:21 -0500

    Added a home page with some potenial plans to add more things

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Signin from './components/Signin/Signin';
+import Home from './components/Home/Home';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Signin />
+        <Home />
       </div>
     );
   }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class Home extends React.Component{
+  render() {
+    return (
+      <div>
+        <div>
+          {/* put nav bar here (ie. home
+          , about
+          , contact
+          , anything else
+          , wouldn't mind putting a news section here kinda like the discord bot you have
+          , maybe a link a live feed of our twitter?) */}
+        </div>
+
+        <div>
+          {/* put logo or name of the site here */}
+        </div>
+
+        <div>
+          {/* display blog posts here */}
+        </div>
+        
+        <div>
+          <h1>Something</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home;
